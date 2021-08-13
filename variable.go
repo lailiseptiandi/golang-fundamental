ef4b87903f0ab2091ced7f6c4884071d1cf7ddc3
laili septiandi <lailiseptiandi2@gmail.com>
2021-08-13 19:46:00 +0700

    fundamental

---
+++ variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var name string = "Laili Septiandi"
+	// var address string 
+	// address = "Sukabumi"
+
+	// fmt.Printf("Halo %s %s!\n", name, address)
+
+	// fmt.Println("Hay", name, address + "!")
+
+	// var firstName = "Laili"
+	// lastName := "Septiandi" //ini bisa dilakukan didalam blok function
+
+	// fmt.Println("Halo", firstName, lastName)
+
+
+	// multi variable
+	var firstName, middleName, lastName string = "laili", "muzzammil", "septiandi"
+
+	fmt.Println(firstName, middleName, lastName)
+
+
+}
+
